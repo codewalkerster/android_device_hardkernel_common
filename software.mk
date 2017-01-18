7a592498ca232ace5eb4de1c56d45e986c9aa191
codewalker <codewalker@hardkernel.com>
2017-01-18 17:40:58 +0900

    ODROID-C2: Fix build bugs.
    Change-Id: I5c94732601a450e3bae3837e6c7dad8e2cf27164

--- software.mk
+++ software.mk
@@ -1,19 +1,5 @@
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.adb.secure=1
 
-ifeq ($(TARGET_BUILD_CTS), true)
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.app.rotation=original
-
-WITH_DEXPREOPT := true
-WITH_DEXPREOPT_PIC := true
-
-PRODUCT_PACKAGES += \
-    Contacts \
-    Bluetooth \
-    DownloadProviderUi \
-    Calendar \
-    QuickSearchBox
-endif
-
