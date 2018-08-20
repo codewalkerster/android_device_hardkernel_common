6a30082b742715b2bbcd499eb1f841875a1cd4c9
Chris KIM <codewalker@hardkernel.com>
2018-08-20 10:31:20 +0900

    ODROID-C3: Support WIFI#5(RTL8812AU)
    Change-Id: Icb5c57ddc652c0fe5eecf7e981ac9f9dc1c808fd

--- wifi_modules.mk
+++ wifi_modules.mk
@@ -1,5 +1,6 @@
 DEFAULT_WIFI_KERNEL_MODULES := \
-	$(PRODUCT_OUT)/obj/lib_vendor/dhd.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/8812au.ko \
+#	$(PRODUCT_OUT)/obj/lib_vendor/dhd.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/bcmdhd.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/btusb.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/8822bu.ko \
