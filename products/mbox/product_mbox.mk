84963d32f1db724a30afb1e5a593075c07e6c6cc
Luke Go <luke.go@hardkernel.com>
2018-11-30 10:41:57 +0900

    ODROID-COMMON: Remove boot animation features of amlogic.
    Change-Id: I41c03a36a262fd99df18cecd679c94bcb35ac9bc

--- products/mbox/product_mbox.mk
+++ products/mbox/product_mbox.mk
@@ -57,20 +57,6 @@ custom_keylayouts := $(wildcard device/hardkernel/common/keyboards/*.kl)
 PRODUCT_COPY_FILES += $(foreach file,$(custom_keylayouts),\
     $(file):$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/$(notdir $(file)))
 
-
-# bootanimation
-ifneq ($(wildcard $(LOCAL_PATH)/bootanimation.zip),)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bootanimation.zip:system/media/bootanimation.zip
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/mbox.mp4:$(TARGET_COPY_OUT_VENDOR)/etc/bootvideo
-
-# default wallpaper for mbox to fix bug 106225
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/default_wallpaper.png:$(TARGET_COPY_OUT_VENDOR)/etc/default_wallpaper.png
-
 # Include BUILD_NUMBER if defined
 VERSION_ID=$(shell find device/*/$(TARGET_PRODUCT) -name version_id.mk)
 ifeq ($(VERSION_ID),)
