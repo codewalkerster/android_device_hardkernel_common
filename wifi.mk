2a91a81f9459e75b33d1a99c0d3987894d26c54e
Chris KIM <codewalker@hardkernel.com>
2019-01-08 17:06:54 +0900

    ODROID-N2: Modify wifi.mk.
    Change-Id: I245eff9ac8b88ad4b1b4cb9826bf41cd25c9a0cb

--- wifi.mk
+++ wifi.mk
@@ -14,42 +14,4 @@
 # limitations under the License.
 #
 
-WIFI_DRIVER             := rtl8812au
-BOARD_WIFI_VENDOR       := realtek
-WIFI_DRIVER_MODULE_PATH := /vendor/lib/modules/8812au.ko
-WIFI_DRIVER_MODULE_NAME := 8812au
-WIFI_DRIVER_MODULE_ARG  := "ifname=wlan0"
-
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_rtl
-
-BOARD_WLAN_DEVICE := rtl8812au
-LIB_WIFI_HAL := libwifi-hal-rtl
-
-WIFI_FIRMWARE_LOADER      := ""
-WIFI_DRIVER_FW_PATH_PARAM := ""
-
-PRODUCT_COPY_FILES += hardware/amlogic/wifi/realtek/config/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf
-
-#PRODUCT_COPY_FILES += device/hardkernel/$(TARGET_PRODUCT)/init.$(TARGET_PRODUCT).wifi_rtk.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.odroid.wifi.rc
-
-PRODUCT_PACKAGES += \
-	wpa_supplicant_overlay.conf \
-
-# 89976: Add Realtek USB WiFi support
-PRODUCT_PROPERTY_OVERRIDES += \
-	wifi.interface=wlan0
-
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
-
-PRODUCT_COPY_FILES += \
-	device/hardkernel/common/wifi/rt2870.bin:root/lib/firmware/rt2870.bin \
-	device/hardkernel/common/wifi/rtl8192cufw_TMSC.bin:root/lib/firmware/rtlwifi/rtl8192cufw_TMSC.bin \
-	device/hardkernel/common/wifi/wifi_id_list.txt:vendor/etc/wifi_id_list.txt \
-	device/hardkernel/common/wifi/8192cu:vendor/etc/modprobe.d/8192cu \
-	device/hardkernel/common/wifi/8812au:vendor/etc/modprobe.d/8812au \
-	device/hardkernel/common/wifi/rt2800usb:vendor/etc/modprobe.d/rt2800usb
+include hardware/amlogic/wifi/configs/wifi.mk
