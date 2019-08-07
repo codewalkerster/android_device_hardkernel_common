114f03d486b6214d9f50b348d02da65844856212
Chris KIM <codewalker@hardkernel.com>
2019-08-07 16:39:41 +0900

    ODROID-N2: Use 8192 driver in hardware instead in kernel.
    Change-Id: I929faf5f1e74eabcd5f6df721eb398df567b530c
    Signed-off-by: Chris KIM <codewalker@hardkernel.com>

--- wifi_modules.mk
+++ wifi_modules.mk
@@ -1,9 +1,6 @@
 DEFAULT_WIFI_KERNEL_MODULES := \
 	$(PRODUCT_OUT)/obj/lib_vendor/8812au.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/rtlwifi.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/rtl_usb.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/rtl8192c-common.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/rtl8192cu.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/8192cu.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/rt2800lib.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/rt2800usb.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/rt2x00lib.ko \
