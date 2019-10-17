976ad091d7280f09b29c0958a4d04926908db106
Chris KIM <codewalker@hardkernel.com>
2019-10-17 16:28:06 +0900

    ODROID: wifi: Support wifi 8821cu module.
    Change-Id: I112c32b76d4a1d3eea1ad20ee2a7c44a4c7df6d6
    Signed-off-by: Chris KIM <codewalker@hardkernel.com>

--- wifi_modules.mk
+++ wifi_modules.mk
@@ -1,5 +1,6 @@
 DEFAULT_WIFI_KERNEL_MODULES := \
 	$(PRODUCT_OUT)/obj/lib_vendor/8812au.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/8821cu.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/8192cu.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/rt2800lib.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/rt2800usb.ko \
