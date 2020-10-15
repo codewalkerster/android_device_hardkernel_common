12eb2c01b63666d51e820c452846e89b8993cc1f
Chris <codewalker@hardkernel.com>
2020-10-15 16:26:23 +0900

    ODROID-HC4: HC4 used c4 uboot build name.
    Change-Id: I42f0ed848b5262e99efac29b6e0e19aa28780171

--- bootloader.mk
+++ bootloader.mk
@@ -6,10 +6,17 @@ $(INSTALLED_BOOTLOADER_TARGET): bootloader/uboot/sd_fuse/u-boot.bin
 	$(hide) cp -a $< $@
 	@echo "make $@: bootloader installed end"
 
+ifeq ($(TARGET_PRODUCT), odroidhc4)
+bootloader/uboot/sd_fuse/u-boot.bin:
+	make -C bootloader/uboot distclean
+	make -C bootloader/uboot odroidc4_config
+	make -C bootloader/uboot bootimage
+else
 bootloader/uboot/sd_fuse/u-boot.bin:
 	make -C bootloader/uboot distclean
 	make -C bootloader/uboot $(TARGET_PRODUCT)_config
 	make -C bootloader/uboot bootimage
+endif
 
 .PHONY: build_bootloader
 build_bootloader: $(INSTALLED_BOOTLOADER_TARGET)
