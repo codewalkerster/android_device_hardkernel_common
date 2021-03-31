b3c4382a435937c1e6323a5c43f7a21090be7f13
Bob Badour <bbadour@google.com>
2021-03-31 12:27:09 -0700

    [LSC] Add LOCAL_LICENSE_KINDS to device/amlogic/common
    Added SPDX-license-identifier-Apache-2.0 to:
      Android.bp
      dolby_ms12/install/Android.mk
      security/Android.mk
      soong_config/Android.bp
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    
    Exempt-From-Owner-Approval: janitorial work
    Change-Id: I906fff9532a9a0db4594a8df1bf525a3b35708ca

--- dolby_ms12/install/Android.mk
+++ dolby_ms12/install/Android.mk
@@ -17,6 +17,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := dolby_fw_dolbyms12
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := bin/$(LOCAL_MODULE)
--- security/Android.mk
+++ security/Android.mk
@@ -5,6 +5,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := verity_key.amlogic
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_SRC_FILES := verity_key
 LOCAL_MODULE_STEM := verity_key
 LOCAL_MODULE_CLASS := ETC
