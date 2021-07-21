65fac54a425d050a902824c51522668b70d33175
Luke go <sangch.go@gmail.com>
2021-07-21 11:42:21 +0900

    ODROID: Support a SD card booting.
    Change-Id: Ieb3aa757ecf515df666f916ee00a8d52e0e13478

--- boot_script/boot.cmd
+++ boot_script/boot.cmd
@@ -1,12 +1,14 @@
-if test "${devtype}" = mmc ; then
+if test "${devnum}" = 0 ; then
 	setenv media emmc
+	setenv boot_device fe310000.sdhci,fe330000.nandc
 else
 	setenv media sd
+	setenv boot_device fe2b0000.dwmmc,fe330000.nandc
 fi
 
 setenv pre_args storagemedia=$media androidboot.storagemedia=$media androidboot.mode=normal androidboot.dtb_idx=0 androidboot.dtbo_idx=0
 
-setenv bootargs ${pre_args} androidboot.boot_devices=fe310000.sdhci,fe330000.nandc swiotlb=1 androidboot.selinux=permissive
+setenv bootargs ${pre_args} androidboot.boot_devices=$boot_device swiotlb=1 androidboot.selinux=permissive
 
 setenv fdt_addr_r 0x0a100000
 setenv kernel_addr_c 0x4080000
