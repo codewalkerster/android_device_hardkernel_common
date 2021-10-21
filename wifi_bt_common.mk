78536d18122d0c7ac79271716a52256b564454c5
Chris <codewalker@hardkernel.com>
2021-10-21 12:09:45 +0900

    ODROID-M1: Fix for wpa_supplicant.
    Change-Id: If3e953e1020d606fa7277ca6fea86388208442ff

--- wifi_bt_common.mk
+++ wifi_bt_common.mk
@@ -11,9 +11,9 @@ WIFI_DRIVER_MODULE_ARG  := "ifname=wlan0"
 BOARD_WIFI_VENDOR := realtek
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_nl80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
 BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_nl80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_rtl
 
 BOARD_WLAN_DEVICE := realtek
 LIB_WIFI_HAL := libwifi-hal-rtl
