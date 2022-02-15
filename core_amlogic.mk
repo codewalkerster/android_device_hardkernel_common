e4fdae74d0590cd0f6141f8495a966050e214902
jiajia tian <jiajia.tian@amlogic.com>
2022-02-15 01:36:22 -0700

    camera: Revert "camera: add external camera [1/1]"
    PD#SWPL-71814
    
    Problem:
    Some 3rd part apps that only support internal camera
    can not work.
    
    Solution:
    Revert this commit, it's just needed for platforms
    provided to google, no need for public platform.
    
    Verify:
    AndroidS ohm
    
    
    This reverts commit 6776a21942aa1825c1d8db2a0d9ea3f4101ec968.
    
    Signed-off-by: jiajia.tian <jiajia.tian@amlogic.com>
    Change-Id: Id38619cae33fd24eb03614f29a0b570dca6d3091

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -465,7 +465,9 @@ endif
 #Camera HAL
 PRODUCT_PACKAGES += \
      camera.amlogic \
-     android.hardware.camera.provider@2.5-external-service
+     android.hardware.camera.provider@2.5-legacy \
+     android.hardware.camera.provider@2.5-service
+
 #Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power.aidl-service.droidlogic
