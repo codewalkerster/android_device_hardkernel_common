56d3ce5598f577f4e253c9ed7e0f481ad2cdaac2
junlu.jiang <junlu.jiang@amlogic.com>
2022-02-23 15:46:29 +0800

    managered_users: add feature [1/1]
    PD#SWPL-71351
    
    Problem:
    management_users is not added
    
    Solution:
    add this feature
    
    Verify:
    androiT
    
    Signed-off-by: junlu.jiang <junlu.jiang@amlogic.com>
    Change-Id: I46eedd9c1f2721a4ffc383bdba9afc282aa92748

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -281,6 +281,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.external.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.external.xml
 
+#add managed_users feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml
+
 ifeq ($(ATV_LAUNCHER), amati)
 # Keymaster configuration
 PRODUCT_COPY_FILES += \
