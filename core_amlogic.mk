7ad05f656d6cc4056c4eaf7884b1d565be02fc36
junlu jiang <junlu.jiang@amlogic.com>
2022-03-07 01:23:29 -0700

    Revert "managered_users: add feature [1/1]"
    This reverts commit 56d3ce5598f577f4e253c9ed7e0f481ad2cdaac2.
    
    Change-Id: I41228047a48170d185725afb1fed0e69d7e10a51

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -281,10 +281,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.external.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.external.xml
 
-#add managed_users feature
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml
-
 ifeq ($(ATV_LAUNCHER), amati)
 # Keymaster configuration
 PRODUCT_COPY_FILES += \
