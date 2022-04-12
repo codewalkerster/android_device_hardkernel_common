b72cec3b57c0753ddfac0e4ff5cf7fc67a1ab3e7
yang liu1 <yang.liu1@amlogic.com>
2022-04-12 15:31:41 +0800

    Android T: support codec2 on ohm. [2/3]
    PD#SWPL-77546
    
    Problem:
    support codec2 on ohm.
    
    Solution:
    support codec2 on ohm.
    
    Verify:
    ohm
    
    Change-Id: Ib9e56ccb9f6802dbda709675bb7eccb0098c8783
    Signed-off-by: yang liu1 <yang.liu1@amlogic.com>

--- products/mbox/sc2/vendor_prop.mk
+++ products/mbox/sc2/vendor_prop.mk
@@ -161,6 +161,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.media.omx2.support_passthrough=true \
     vendor.media.omx.secure.prealloc=true
 
+#codec2
+ifeq ($(VENDOR_MEDIA_CODEC2_SUPPORT),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.codec2.support=true \
+    vendor.media.codec2.disable_secure=false \
+    debug.stagefright.ccodec_delayed_params=true \
+    debug.c2.use_dmabufheaps=1
+endif
 
 #use dv frame mode
 PRODUCT_PROPERTY_OVERRIDES += \
