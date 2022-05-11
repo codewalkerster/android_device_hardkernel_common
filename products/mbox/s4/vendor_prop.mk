0d58e49d8e5af5d3c9d95cb434efdb12aa0ab30b
yang liu1 <yang.liu1@amlogic.com>
2022-05-11 15:56:56 +0800

    Android T: CF2 support no-surface stream playback. [1/2]
    PD#SWPL-80364
    
    Problem:
    support no-surface stream playback.
    
    Solution:
    1.add property for no-surface buffer pool.
    2.implementation buffer pool for no-surface stream.
    
    Verify:
    ohm
    
    Change-Id: If7db6cea44dff91dd3a9686efff42307447edc38
    Signed-off-by: yang liu1 <yang.liu1@amlogic.com>

--- products/mbox/s4/vendor_prop.mk
+++ products/mbox/s4/vendor_prop.mk
@@ -172,6 +172,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.media.codec2.support=true \
     vendor.media.codec2.disable_secure=false \
     debug.stagefright.ccodec_delayed_params=true \
+    debug.stagefright.c2-poolmask=458752 \
     debug.c2.use_dmabufheaps=1
 endif
 
