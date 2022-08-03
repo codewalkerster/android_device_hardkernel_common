12abf4d3388e1926a914f2bdf1909f8a63e21c02
wanwei.jiang <wanwei.jiang@amlogic.com>
2022-08-03 22:24:08 +0800

    module: android driver loading sequence [1/1]
    PD#SWPL-89845
    
    Problem:
    can't enter recovery mode
    
    Solution:
    move some drivers to vendor_boot, such as drm, key
    
    Verify:
    sc2
    
    Change-Id: I9f5459d491d90f096f7591ddf4de61347805341f
    Signed-off-by: wanwei.jiang <wanwei.jiang@amlogic.com>

--- build_kernel_modules.mk
+++ build_kernel_modules.mk
@@ -48,12 +48,12 @@ BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PRODUCT_PATH)-kernel/$(TARGET_KERNEL_DIR)/d
 ifeq ($(TARGET_BUILD_KERNEL_VERSION),5.15)
 RAMDISK_KERNEL_MODULES_LOAD_FIRSTLIST := $(strip $(shell cat $(DEVICE_PRODUCT_PATH)-kernel/$(TARGET_KERNEL_DIR)/vendor_boot.modules.load))
 ifeq ($(strip $(RAMDISK_KERNEL_MODULES_LOAD_FIRSTLIST)),)
-	$(error vendor_boot.modules.load not found or empty)
+$(warning "vendor_boot.modules.load not found or empty")
 endif
 
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PRODUCT_PATH)-kernel/$(TARGET_KERNEL_DIR)/vendor_dlkm.modules.load))
 ifeq ($(strip $(BOARD_VENDOR_KERNEL_MODULES_LOAD)),)
-	$(error vendor_dlkm.modules.load not found or empty)
+$(warning "vendor_dlkm.modules.load not found or empty")
 endif
 
 RAMDISK_KERNEL_MODULES_LOAD_EXCLUDELIST +=
--- kernelbuild/build_kernel_5.15.sh
+++ kernelbuild/build_kernel_5.15.sh
@@ -25,18 +25,17 @@ rm -rf device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/lib/modules/*
 cp ${DIST_DIR}/modules/vendor/*.ko device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/lib/modules/
 cp -a ${COMMON_OUT_DIR}/vendor_lib/* device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/lib/
 
-# awk '{print $0" \\"}' ${DIST_DIR}/modules/ramdisk/ramdisk_modules.order > device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_boot.modules.load
-awk '{print $0}' ${DIST_DIR}/modules/ramdisk/ramdisk_modules.order > device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_boot.modules.load
-echo "" >> device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_boot.modules.load
+cp ${DIST_DIR}/modules/ramdisk/ramdisk_modules.order device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_boot.modules.load
 
-# awk '{print $0" \\"}' ${DIST_DIR}/modules/vendor/vendor_modules.order > device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
-awk '{print $0}' ${DIST_DIR}/modules/vendor/vendor_modules.order > device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
+cp ${DIST_DIR}/modules/vendor/vendor_modules.order device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
 if [[ -n ${LOAD_EXT_MODULES_IN_SECOND_STAGE} ]]; then
 	cp ${DIST_DIR}/ext_modules/*.ko device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/lib/modules/
-#	awk '{print $0" \\"}' ${DIST_DIR}/ext_modules/ext_modules.order >> device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
-	awk '{print $0}' ${DIST_DIR}/ext_modules/ext_modules.order >> device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
+	cat ${DIST_DIR}/ext_modules/ext_modules.order >> device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
 fi
-echo "" >> device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
+
+head -n 60 device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load >> device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_boot.modules.load
+sed -i "1,60d" device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/vendor_dlkm.modules.load
+cp ${DIST_DIR}/modules/vendor/*.ko device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/ramdisk/lib/modules/
 
 cp ${DIST_DIR}/dtbo.img device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/
 cp ${DIST_DIR}/${KERNEL_DEVICETREE}.dtb device/amlogic/${BOARD_DEVICENAME}-kernel/${KERNEL_VERSION}/${BOARD_DEVICENAME}.dtb
