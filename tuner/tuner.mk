23bcc927df6ae0d59d5e55baca5ea400b8224ec4
min.yang <min.yang@amlogic.com>
2022-09-05 20:12:32 +0800

    Tuner: update tuner ko in 5.15 [1/3]
    PD#SWPL-92924
    
    Problem:
    All tuner ko need work in kernel 5.15
    
    Solution:
    update
    
    Verify:
    verified by T + ah212
    
    Change-Id: I522527eba27250ddc402e61e55381816fb324969
    Signed-off-by: min.yang <min.yang@amlogic.com>

--- tuner/tuner.mk
+++ tuner/tuner.mk
@@ -29,6 +29,12 @@ PRODUCT_COPY_FILES += $(foreach tuner, $(TUNER_MODULE),\
         device/amlogic/common/tuner/32_4_9/$(tuner)_fe_32.ko:$(PRODUCT_OUT)/obj/lib_vendor/$(tuner)_fe.ko,\
         device/amlogic/common/tuner/64_4_9/$(tuner)_fe_64.ko:$(PRODUCT_OUT)/obj/lib_vendor/$(tuner)_fe.ko)\
         device/amlogic/common/initscripts/tuner/$(tuner)_fe.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/$(tuner)_fe.rc)
+else ifeq ($(TARGET_BUILD_KERNEL_VERSION),5.15)
+PRODUCT_COPY_FILES += $(foreach tuner, $(TUNER_MODULE),\
+    $(if $(findstring true, $(KERNEL_A32_SUPPORT)),\
+        device/amlogic/common/tuner/32_5_15/$(tuner)_fe_32.ko:$(PRODUCT_OUT)/obj/lib_vendor/$(tuner)_fe.ko,\
+        device/amlogic/common/tuner/64_5_15/$(tuner)_fe_64.ko:$(PRODUCT_OUT)/obj/lib_vendor/$(tuner)_fe.ko)\
+        device/amlogic/common/initscripts/tuner/$(tuner)_fe.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/$(tuner)_fe.rc)
 else
 PRODUCT_COPY_FILES += $(foreach tuner, $(TUNER_MODULE),\
     $(if $(findstring true, $(KERNEL_A32_SUPPORT)),\
