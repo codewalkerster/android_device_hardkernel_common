056be9b53f81150e0a2688d9b670bfbe79cc7587
Song Zhang <song.zhang@amlogic.com>
2022-09-26 16:27:45 +0800

    t7_an400: enable ccodec on android t [1/1]
    PD#SWPL-94120
    
    Problem:
    enable ccodec on android t.
    
    Solution:
    enable ccodec on android t.
    
    Verify:
    t7_an400
    
    Signed-off-by: Song Zhang <song.zhang@amlogic.com>
    Change-Id: Iea6c9430c3187f3feb66c1530bd8a5fa765c88f7

--- products/tv/t7/vendor_prop.mk
+++ products/tv/t7/vendor_prop.mk
@@ -185,6 +185,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.media.omx.dibypass.enable=false \
     vendor.media.omx.videolayerrotation.enable=false \
 
+#codec2
+ifeq ($(VENDOR_MEDIA_CODEC2_SUPPORT),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.codec2.support=true \
+    vendor.media.codec2.disable_secure=false \
+    debug.stagefright.ccodec_delayed_params=true \
+    debug.c2.use_dmabufheaps=1
+endif
+
 #usb controller
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.usb.controller=fdd00000.crgudc2
