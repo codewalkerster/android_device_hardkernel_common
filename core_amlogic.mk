f58b7ddf3f42089b0606fe4ac4294ad57d790c7e
Liang Ji <liang.ji@amlogic.com>
2022-11-08 10:29:37 +0800

    cts: CtsKeystoreTestCases  failed on atv [1/1]
    PD#SWPL-100533
    
    Problem:
    android.keystore.cts.NoAttestKeyTest#testEcAttestKeyFail testcase
    is failed
    
    Solution:
    add android.hardware.keystore.app_attest_key.xml for atv projects
    
    Verify:
    ohm-atv
    
    Signed-off-by: Liang Ji <liang.ji@amlogic.com>
    Change-Id: I6336eccbf0f3c88251f6fd8d1e00ac61c1306d61

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -269,10 +269,12 @@ ifeq ($(ATV_LAUNCHER), amati)
 # Keymaster configuration
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
-    frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml \
+    frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml
+endif
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     hardware/amlogic/keymaster/keymint/rkp_extract.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/rkp_extract.rc
-endif
 
 #amlogic HALs
 PRODUCT_PACKAGES += \
