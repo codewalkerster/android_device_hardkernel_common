3afe587777445c4ab302e5b76cdc72acc6b34586
Jian Cao <jian.cao@amlogic.com>
2022-12-19 16:16:35 +0800

    gdc: gdc fw support android [1/1]
    PD#SWPL-100390
    
    Problem:
    gdc fw support android.
    
    Solution:
    gdc fw support android.
    
    Verify:
    T7
    
    Change-Id: Ib2e1e19460179c2e1f3ec7f063927a17e9b01fc2
    Signed-off-by: Jian Cao <jian.cao@amlogic.com>

---
+++ video_algorithm/gdc/gdc.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#======================================================================================
+# 1.for gdc_alg ko file copy
+#======================================================================================
+
+ifeq ($(strip $(GDC_FW_MODULE)),true)
+    $(warning GDC_FW_MODULE is $(GDC_FW_MODULE))
+    ifeq ($(TARGET_BUILD_KERNEL_4_9),true)
+
+    else
+        ifeq ($(KERNEL_A32_SUPPORT),true)
+            PRODUCT_COPY_FILES += \
+                device/amlogic/common/video_algorithm/gdc/32/gdc_fw.ko:$(PRODUCT_OUT)/obj/lib_vendor/gdc_fw.ko \
+                device/amlogic/common/initscripts/gdc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/gdc.rc
+        else
+            PRODUCT_COPY_FILES += \
+                device/amlogic/common/video_algorithm/gdc/64/gdc_fw.ko:$(PRODUCT_OUT)/obj/lib_vendor/gdc_fw.ko \
+                device/amlogic/common/initscripts/gdc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/gdc.rc
+        endif
+    endif
+endif
