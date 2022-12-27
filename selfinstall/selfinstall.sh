79400d6b22df1a1484db0eeb09b70b49b9f890eb
Luke go <sangch.go@gmail.com>
2022-12-27 14:09:35 +0900

    ODROID-M1: selfinstall: Apply typecode.
    - Add typecode option to sgdisk command.
    
    Signed-off-by: Luke go <sangch.go@gmail.com>
    Change-Id: Ie05c6232a30cb5e010079ab450d4c26c009b51b1

--- selfinstall/selfinstall.sh
+++ selfinstall/selfinstall.sh
@@ -23,15 +23,15 @@ dd if=$TARGET_PATH/super.img of=$TARGET_IMAGE bs=512 seek=2473984
 dd if=/dev/zero of=$TARGET_IMAGE bs=512 seek=8847360 count=34
 
 $SGDISK_HOST \
-	--n=1:2048:40959 --change-name=1:fat \
+	--n=1:2048:40959 --change-name=1:fat --typecode=1:0700 \
 	--n=2:40960:49151 --change-name=2:security \
-	--n=3:49152:53247 --change-name=3:uboot \
-	--n=4:53248:61439 --change-name=4:misc \
+	--n=3:49152:53247 --change-name=3:uboot --typecode=3:a000 \
+	--n=4:53248:61439 --change-name=4:misc --typecode=4:a004 \
 	--n=5:61440:63487 --change-name=5:vbmeta \
-	--n=6:63488:145407 --change-name=6:boot \
-	--n=7:145408:342015 --change-name=7:recovery \
-	--n=8:342016:2439167 --change-name=8:cache \
-	--n=9:2439168:2471935 --change-name=9:metadata \
+	--n=6:63488:145407 --change-name=6:boot --typecode=6:a002 \
+	--n=7:145408:342015 --change-name=7:recovery --typecode=7:a003 \
+	--n=8:342016:2439167 --change-name=8:cache --typecode=8:a007 \
+	--n=9:2439168:2471935 --change-name=9:metadata --typecode=9:a005 \
 	--n=10:2471936:2473983 --change-name=10:baseparameter \
 	--n=11:2473984:8847359 --change-name=11:super \
 	$TARGET_IMAGE
