58a9e1797919e09f336553229a5441f1b2392eb3
Wencai You <wencai.you@amlogic.com>
2022-12-28 14:45:13 +0800

    common: Pre-Allocate memory in playback [1/1]
    PD#SWPL-100052
    
    Problem:
    We have optimized CMA memory usage,
    Pre-Allocate memory in playback
    
    Solution:
    change vendor.media.c2.secure.prealloc
    
    Verify:
    oppen
    
    Change-Id: I1d90f5775e8055a07bda615cd56019cd9075696d
    Signed-off-by: Wencai You <wencai.you@amlogic.com>

--- products/mbox/s4/vendor_prop.mk
+++ products/mbox/s4/vendor_prop.mk
@@ -161,7 +161,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.media.codec2.disable_secure=false \
     debug.stagefright.c2-poolmask=458752 \
     debug.c2.use_dmabufheaps=1 \
-    vendor.media.c2.vdec.enable_h264_4k_mmu=true
+    vendor.media.c2.vdec.enable_h264_4k_mmu=true \
+    vendor.media.c2.secure.prealloc=true
 endif
 
 #use dv frame mode
