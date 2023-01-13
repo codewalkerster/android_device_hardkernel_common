3fef4b348c2a596f1ad7dcb77d7d0a088637137d
jinbing.zhu <jinbing.zhu@amlogic.com>
2023-01-13 17:20:24 +0800

    device: 5.15 insmod algorithm ko [1/1]
    PD#SWPL-108642
    
    Problem:
    insmod new ko
    
    Solution:
    insmod new ko
    
    Verify:
    t982
    
    Change-Id: Ieb8b6ecb19e99d918ade78639c13f87373c26a89
    Signed-off-by: jinbing.zhu <jinbing.zhu@amlogic.com>

---
+++ video_algorithm/algorithm/algorithm.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#======================================================================================
+# 1.for amlogic-algorithm ko file copy
+#======================================================================================
+
+ifeq ($(strip $(ALGORITHM_MODULE)),true)
+    $(warning ALGORITHM_MODULE is $(ALGORITHM_MODULE))
+    ifeq ($(KERNEL_A32_SUPPORT),true)
+        PRODUCT_COPY_FILES += \
+            device/amlogic/common/video_algorithm/algorithm/32/amlogic-algorithm_32.ko:$(PRODUCT_OUT)/obj/lib_vendor/amlogic-algorithm.ko \
+            device/amlogic/common/initscripts/amlogic-algorithm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/amlogic-algorithm.rc
+    else
+        PRODUCT_COPY_FILES += \
+            device/amlogic/common/video_algorithm/algorithm/64/amlogic-algorithm_64.ko:$(PRODUCT_OUT)/obj/lib_vendor/amlogic-algorithm.ko \
+            device/amlogic/common/initscripts/amlogic-algorithm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/amlogic-algorithm.rc
+    endif
+endif
