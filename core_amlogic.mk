8df8466bf037d27c8fa0bd32c6a547e14f2c8890
Cheng Wang2 <cheng.wang2@amlogic.com>
2023-02-01 18:01:05 +0800

    Audio: Android 64bit support [1/1]
    PD#SWPL-109583
    
    Problem:
    no 64bit audio hal
    
    Solution:
    remove 32bit only flag
    
    Verify:
    t7c
    
    Change-Id: I30248cc17dfbc66031743d6b903a1a84d4a17081
    Signed-off-by: Cheng Wang2 <cheng.wang2@amlogic.com>

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -469,21 +469,21 @@ PRODUCT_PACKAGES += \
 
 #Audio HAL
 PRODUCT_PACKAGES += \
-     android.hardware.audio@6.0-impl:32 \
-     android.hardware.audio@5.0-impl:32 \
-     android.hardware.audio@4.0-impl:32 \
-     android.hardware.audio.effect@6.0-impl:32 \
-     android.hardware.audio.effect@5.0-impl:32 \
-     android.hardware.audio.effect@4.0-impl:32 \
-     android.hardware.audio@2.0-impl:32 \
-     android.hardware.audio.effect@2.0-impl:32 \
+     android.hardware.audio@6.0-impl \
+     android.hardware.audio@5.0-impl \
+     android.hardware.audio@4.0-impl \
+     android.hardware.audio.effect@6.0-impl \
+     android.hardware.audio.effect@5.0-impl \
+     android.hardware.audio.effect@4.0-impl \
+     android.hardware.audio@2.0-impl \
+     android.hardware.audio.effect@2.0-impl \
      android.hardware.audio@2.0-service-droidlogic
 
 ifneq ($(TARGET_BUILD_KERNEL_VERSION),4.9)
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl:32 \
-    android.hardware.audio@7.0-impl:32 \
-    android.hardware.audio.effect@7.0-impl:32
+    android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl
 endif
 
 #Camera HAL
