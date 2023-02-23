bb0cb16626d487ebc0bc1ccef4deb75c4a51d4df
yujie.wu <yujie.wu@amlogic.com>
2023-02-23 15:13:30 +0800

    audio: put ac4 media codec xml into oem.img by default [2/2]
    PD#SWPL-112809
    
    Problem:
    If ac4 media codec xml is enabled by default, it will
    causes xts error for none ms12 version
    
    Solution:
    put it into oem image, and only ms12 v2 version will
    have such config
    
    Verify:
    ohm
    
    Change-Id: I7b5a9fff3e2fc63978176a022d9bcbcc24d1f23f
    Signed-off-by: yujie.wu <yujie.wu@amlogic.com>

--- licensebuild/Android.mk
+++ licensebuild/Android.mk
@@ -31,6 +31,7 @@ LOCAL_POST_INSTALL_CMD += \
     ln -sf /vendor/etc/audio_policy_volumes.xml $(TARGET_OUT_ODM)/etc/audio_policy_volumes.xml; \
     ln -sf /oem/etc/media_codecs_amlogic_dolby_vision.xml $(TARGET_OUT_VENDOR)/etc/media_codecs_amlogic_dolby_vision.xml; \
     ln -sf /oem/etc/media_codecs_amlogic_performance_dolby_vision.xml $(TARGET_OUT_VENDOR)/etc/media_codecs_amlogic_performance_dolby_vision.xml; \
+    ln -sf /oem/etc/media_codecs_amlogic_audio_ac4.xml $(TARGET_OUT_VENDOR)/etc/media_codecs_amlogic_audio_ac4.xml; \
     ln -sf /oem/etc/firmware/firmware.le $(TARGET_OUT_ODM)/etc/firmware/firmware.le;
 
 include $(BUILD_PREBUILT)
