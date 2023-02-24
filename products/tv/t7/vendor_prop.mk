01af64cae76d9a8820985f62ad56ab060bf5b33c
longhai.huang <longhai.huang@amlogic.com>
2023-02-24 20:20:14 +0800

    T7c: add 4k h264 mmu [1/1]
    PD#SWPL-112948
    
    Problem:
    there is no 4k h264 mmu
    
    Solution:
    add 4k h264 mmu
    
    Verify:
    T7c
    
    Change-Id: Ifb27f58b2d645439ee2ecfbea0295b8efe4a9408
    Signed-off-by: longhai.huang <longhai.huang@amlogic.com>

--- products/tv/t7/vendor_prop.mk
+++ products/tv/t7/vendor_prop.mk
@@ -174,13 +174,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.ionvideo.enable=1 \
     vendor.media.omx.dibypass.enable=false \
     vendor.media.omx.videolayerrotation.enable=false \
+    vendor.media.omx.dec.enable_h264_4k_mmu=true
 
 #codec2
 ifeq ($(VENDOR_MEDIA_CODEC2_SUPPORT),true)
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.media.codec2.support=true \
     vendor.media.codec2.disable_secure=false \
-    debug.c2.use_dmabufheaps=1
+    debug.c2.use_dmabufheaps=1\
+    vendor.media.c2.vdec.enable_h264_4k_mmu=true
 endif
 
 #usb controller
