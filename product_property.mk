95a433b0da6857000a258765f18bd3baf9028f15
fuqing.chen <fuqing.chen@amlogic.com>
2023-03-23 13:50:12 +0800

    property: disable frame rate override [1/1]
    PD#SWPL-112613
    
    Problem:
    cts test FrameRateOverrideHostTest failed
    
    Solution:
    disable frame rate override property
    
    Verify:
    adt4
    
    Change-Id: Ibeba89a5b9464eaa44b5a0fdfadf2f93848c700b
    Signed-off-by: fuqing.chen <fuqing.chen@amlogic.com>

--- product_property.mk
+++ product_property.mk
@@ -8,6 +8,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.vsync_reactor_ignore_present_fences=1 \
     debug.sf.enable_gl_backpressure=0
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.enable_frame_rate_override=false
 
 # config of surfaceflinger
 ifneq ($(CONFIG_DEVICE_LOW_RAM),true)
