591d7e02aa12a080f7bfa1c05dd6fba17b266ca1
yafeng.zhao <yafeng.zhao@amlogic.com>
2023-04-06 14:46:16 +0800

    device: CF2 Enable Codec2 H264 4K mmu for S5. [1/1]
    PD#SWPL-118347
    
    Problem:
    Support H264 4K mmu
    
    Solution:
    Added vendor.media.omx.dec.enable_h264_4k_mmu to enable the feature
    globally,
    When playing 4K H264 stream, with the property above enabled, omx will
    use
    double write mode 3 for decoder driver to save bandwidth.
    
    Verify:
    S5
    Android-T
    
    Change-Id: I8d2608e252dea47e24718dfe2f6c28413410475e
    Signed-off-by: yafeng.zhao <yafeng.zhao@amlogic.com>

--- products/mbox/s5/vendor_prop.mk
+++ products/mbox/s5/vendor_prop.mk
@@ -159,14 +159,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.omx2.nr.enable=true \
     vendor.omx2.di.localbuf.enable=true \
     vendor.media.omx2.support_passthrough=true \
-    vendor.media.omx2.1080p_buffer=true
+    vendor.media.omx2.1080p_buffer=true \
+    vendor.media.omx.dec.enable_h264_4k_mmu=true
+
+#codec2
+ifeq ($(VENDOR_MEDIA_CODEC2_SUPPORT),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.codec2.support=true \
+    vendor.media.codec2.disable_secure=false \
+    debug.c2.use_dmabufheaps=1 \
+    vendor.media.c2.vdec.enable_h264_4k_mmu=true
+
+endif
 
 #use dv frame mode
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.media.omx.use_dv_frame=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-     ro.vendor.platform.is.dualspdif=true
+    ro.vendor.platform.is.dualspdif=true
 
 #support hardware av1 decoder
 PRODUCT_PROPERTY_OVERRIDES += \
