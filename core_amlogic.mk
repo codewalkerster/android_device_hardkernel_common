bfcebe9a470ae35134cb67ff55c3b2f20fbf5fc9
Lele Liu <lele.liu@amlogic.com>
2023-04-25 04:34:27 -0700

    Revert "widevine: CF2 Add apex support for widevine [1/1]"
    This reverts commit 45cc093d0219e9d8584bfa5d9f67471156977c84.
    
    Change-Id: If2a8b63f1141c196bd232ef1ad64bd4351e0c911

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -550,16 +550,10 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PACKAGES += \
+    android.hardware.drm-service.widevine \
     android.hardware.drm-service.clearkey \
     move_widevine_data.sh \
     android.hardware.drm@1.4-service.playready
-
-ifneq ($(BOARD_COMPILE_ATV), false)
-include vendor/widevine/libwvdrmengine/apex/device/device-nonupdatable.mk
-else
-PRODUCT_PACKAGES += \
-    android.hardware.drm-service.widevine
-endif
 
 # HDMITX CEC HAL
 PRODUCT_PACKAGES += \
--- media.mk
+++ media.mk
@@ -312,12 +312,8 @@ ifeq ($(TARGET_USE_SECUREOS), true)
 endif
 endif
 
-ifeq ($(BOARD_COMPILE_ATV), false)
 PRODUCT_PACKAGES += \
-    libwvaidl
-endif
-
-PRODUCT_PACKAGES += \
+    libwvaidl \
     liboemcrypto \
     e043cde0-61d0-11e5-9c26-0002a5d5c51b
 
