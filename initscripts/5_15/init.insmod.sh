838818a142f6e9558cde97e9a9b6b815e623c2ec
Wanwei Jiang <wanwei.jiang@amlogic.com>
2023-09-10 23:15:35 +0800

    script: support dailybuild and release gki image [2/3]
    PD#SWPL-139596
    
    Problem:
    the priority loading strategy for gki is not good,
    which will change the pre-set loading order
    
    Solution:
    the gki module and vendor module have the same loading priority
    
    Verify:
    ohm, oppen, planck
    
    Change-Id: I6dd9eccba96f82222d9bd2f60a3dc74237ec6fca
    Signed-off-by: Wanwei Jiang <wanwei.jiang@amlogic.com>

--- initscripts/5_15/init.insmod.sh
+++ initscripts/5_15/init.insmod.sh
@@ -1,13 +1,11 @@
 #!/vendor/bin/sh
 
 system_modules_dir=$(echo /system/lib/modules/5.15*)
-for module in `cat /vendor/lib/modules/system_dlkm.modules.load`
-do
-    insmod ${system_modules_dir}/${module}
+for module in `cat /vendor/lib/modules/modules.load`; do
+	system_module=`grep "/${module}" vendor/lib/modules/system_dlkm.modules.load`
+	if [ -f ${system_modules_dir}/${system_module} ]; then
+		 insmod ${system_modules_dir}/${system_module}
+	else
+		insmod /vendor/lib/modules/${module}
+	fi
 done
-
-for module in `cat /vendor/lib/modules/modules.load`
-do
-    insmod /vendor/lib/modules/$module
-done
-
