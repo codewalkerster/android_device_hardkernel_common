d90e713f2637034970ef5ef78cc0892278e9d0c9
Lele Liu <lele.liu@amlogic.com>
2023-10-25 15:12:29 +0800

    Android U: integrate atvalex [1/1]
    PD#SWPL-143595
    
    Problem:
    short of gtvs apk atvalex
    
    Solution:
    just integrate atvalex
    
    Verify:
    local
    
    Change-Id: I07dfe88b90f348d96754ed41a306d91af50340ed
    Signed-off-by: Lele Liu <lele.liu@amlogic.com>

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -955,3 +955,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Disable Prime Shader Cache in SurfaceFlinger to make it available faster
 PRODUCT_PROPERTY_OVERRIDES += \
     service.sf.prime_shader_cache=0
+
+###########################AtvAxel########################################
+PRODUCT_PACKAGES += \
+     AtvAxel
