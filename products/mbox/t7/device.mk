bfe4c85fe5c08f2e5ce5affab1ca863c73aad9bd
Luan Yuan <luan.yuan@amlogic.com>
2023-11-22 16:41:28 +0800

    bluebell: change bluebell config from TV to Mbox. [1/2]
    PD#SWPL-147037
    
    Problem:
    bluebell is mbox, so need optimize the config.
    
    Solution:
    optimize the config.
    
    Verify:
    verify by bluebell.
    
    Change-Id: I568d2b8b2c4968297c3eaca17ee740c2adf02f98
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

---
+++ products/mbox/t7/device.mk
@@ -0,0 +1,116 @@
+#
+# Copyright (C) 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+CHIP_DIR := device/amlogic/common/products/mbox/t7
+
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/products/mbox/Vendor_1b8e_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1b8e_Product_0001.kl \
+    device/amlogic/common/products/mbox/Vendor_1915_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1915_Product_0001.kl
+
+#########################################################################
+#
+# Recovery
+#
+#########################################################################
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/recovery/busybox:recovery/root/sbin/busybox \
+    $(CHIP_DIR)/recovery/sh:recovery/root/sbin/sh
+
+#########################################################################
+#
+# Init config
+#
+#########################################################################
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/products/mbox/init.amlogic.system.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.rc
+
+ifneq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/recovery/init.recovery.amlogic.rc:recovery/root/init.recovery.amlogic.rc
+else
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/recovery/init.recovery.amlogic_ab.rc:recovery/root/init.recovery.amlogic.rc
+endif
+
+#########################################################################
+#
+# Media codec
+#
+#########################################################################
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    $(CHIP_DIR)/files/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
+#HDCP 1.4 file
+#PRODUCT_COPY_FILES += \
+#    $(CHIP_DIR)/files/tv/dec:$(TARGET_COPY_OUT_ODM)/bin/dec
+
+# tv config file
+TVCONFIG_FILES := \
+    $(CHIP_DIR)/files/tv/tvconfig/*
+PQ_FILES := \
+    $(CHIP_DIR)/files/PQ/pq.db \
+    $(CHIP_DIR)/files/PQ/overscan.db \
+    $(CHIP_DIR)/files/PQ/pq_default.ini
+
+#thermal 2.0 config file
+ifeq ($(TARGET_BUILD_KERNEL_VERSION),5.15)
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/thermal_info_config_5_15.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+else
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+endif
+#########################################################################
+#
+# Audio
+#
+#########################################################################
+ifneq ($(TARGET_BUILD_OEM_WITH_LICENSE_FILES), true)
+ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
+AUDIO_FEATURE_TYPE :=
+ifeq ($(TARGET_BUILD_DOLBY_MS12_V2),true)
+AUDIO_FEATURE_TYPE := $(AUDIO_FEATURE_TYPE)_ms12
+endif
+
+ifeq ($(TARGET_BUILD_DOLBY_MS12_V1),true)
+AUDIO_FEATURE_TYPE := $(AUDIO_FEATURE_TYPE)_ms12_v1
+endif
+
+ifeq ($(TARGET_BUILD_DOLBY_DDP),true)
+AUDIO_FEATURE_TYPE := $(AUDIO_FEATURE_TYPE)_ddp
+endif
+
+ifeq ($(TARGET_BUILD_DTSHD),true)
+AUDIO_FEATURE_TYPE := $(AUDIO_FEATURE_TYPE)_dtshd
+endif
+
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/audio/$(PRODUCT_TYPE)/audio_policy_configuration$(AUDIO_FEATURE_TYPE).xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+endif  ###end USE_XML_AUDIO_POLICY_CONF
+else
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/audio/$(PRODUCT_TYPE)/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+endif  ###end TARGET_BUILD_OEM_WITH_LICENSE_FILES
+#########################################################################
+#
+# tunerhal
+#
+#########################################################################
+ifeq ($(SUPPORT_TUNERHAL), true)
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/tunerhal/frontendinfos.json:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_hal/frontendinfos.json
+endif
---
+++ products/mbox/t7/vendor_prop.mk
@@ -0,0 +1,236 @@
+# Copyright (C) 2011 Amlogic Inc
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for Meson reference board.
+#
+
+# Set display related config
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.has.mbxuimode=true \
+    ro.vendor.platform.has.realoutputmode=true \
+    ro.vendor.platform.need.display.hdmicec=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.camera_preview.maxsize=1920x1080 \
+    ro.media.camera_preview.limitedrate=1920x1080x30,1280x720x30,640x480x30,320x240x28 \
+    ro.vendor.platform.usehwmjpeg=true \
+    vendor.media.camera.dec.mediahalsdk=true
+
+#media
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.media.dv.standalone.component=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.camera_usb.faceback=true
+
+# t962x3_ab301 support screen capture
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.screencontrol.porttype=1 \
+    ro.vendor.screencontrol.maxbufsize=314572800
+
+#if wifi Only
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.radio.noril=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.media_vol_steps=100
+
+#if need pppoe
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.net.pppoe=true
+
+#the prop is used for enable or disable
+#DD+/DD force output when HDMI EDID is not supported
+#by default,the force output mode is enabled.
+#Note,please do not set the prop to true by default
+#only for netflix,just disable the feature.so set the prop to true
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.disable.audiorawout=false
+
+#Dolby DD+ decoder option
+#this prop to for videoplayer display the DD+/DD icon when playback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.support.dolby=true
+#DTS decoder option
+#display dts icon when playback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.support.dts=true
+#DTS-HD support prop
+#PRODUCT_PROPERTY_OVERRIDES += \
+    #ro.vendor.platform.support.dtstrans=true \
+    #ro.vendor.platform.support.dtsmulasset=true
+#DTS-HD prop end
+# Enable player buildin
+
+#platform support dolby vision
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.support.dolbyvision=true \
+    vendor.media.support.dolbyvision = true
+
+#add for video boot, 1 means use video boot, others not .
+PRODUCT_PROPERTY_OVERRIDES += \
+    service.bootvideo=0
+
+# Define drm for this device
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=1
+
+#enable/disable afbc
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.afbcd.enable=1
+
+#set memory upper limit for extractor process
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.media.maxmem=629145600
+
+#map volume
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.mapvalue=0,0,0,0
+
+#adb
+PRODUCT_PROPERTY_OVERRIDES += \
+    service.adb.tcp.port=5555
+
+
+
+#disable timeshift
+PRODUCT_PROPERTY_OVERRIDES += \
+    tv.dtv.tf.disable=true
+
+# crypto volume
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.volume.filenames_mode=aes-256-cts
+
+#this property is used for Android TV audio
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.vendor.platform.is.tv=1
+
+#bootvideo
+#0                      |050
+#^                      |
+#|                      |
+#0:bootanim             |
+#1:bootanim + bootvideo |
+#2:bootvideo + bootanim |
+#3:bootvideo            |
+#others:bootanim        |
+#-----------------------|050
+#050:default volume value, volume range 0~100
+#note that the high position 0 can not be omitted
+ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.media.bootvideo=0050
+endif
+
+# platform digital tv standards
+# atsc/dvb/isdb/sbtvd
+# ro.vendor.platform.digitaltv.standards
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.digitaltv.standards=atsc
+
+#support hardware av1 decoder
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.support.av1=true
+
+#support 4k
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.amplayer.videolimiter=true \
+    ro.vendor.platform.support.4k=true
+
+
+#used for controlling reference board's preview window,
+#project's need disable it or can refer its implementation method.
+ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    tv.need.droidlogic.preview_window=true
+endif
+
+#ifeq ($(TARGET_BUILD_GOOGLE_ATV), false)
+#USB wifi need to be disabled when suspending
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.vendor.platform.wifi.suspend=false
+#endif
+
+# for playback of audio offload
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.offload.video=true \
+    audio.offload.min.duration.secs=5
+
+# hwui
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=40.5f \
+    ro.hwui.layer_cache_size=33.75f
+
+#support video_composer
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.omx.display_mode=3
+
+
+#omx2
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.media.support.omx2=true \
+    vendor.media.omx.use.omx2=true \
+    vendor.media.omx2.in_buffer=5 \
+    vendor.media.omx2.support_mpeg=true \
+    vendor.ionvideo.enable=1 \
+    vendor.media.omx.dibypass.enable=false \
+    vendor.media.omx.videolayerrotation.enable=false \
+    vendor.media.omx.dec.enable_h264_4k_mmu=true
+
+#codec2
+ifeq ($(VENDOR_MEDIA_CODEC2_SUPPORT),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.codec2.support=true \
+    vendor.media.codec2.disable_secure=false \
+    debug.c2.use_dmabufheaps=1\
+    debug.stagefright.c2inputsurface=-1 \
+    debug.vendor.media.c2.vdec.support_10bit=false \
+    vendor.media.c2.disp.nr.enable=true \
+    vendor.media.c2.disp.di.localbuf_enable=true \
+    vendor.media.c2.vdec.enable_h264_4k_mmu=true \
+    vendor.media.common.fixed_buffer_slice=1080
+endif
+
+#usb controller
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.usb.controller=fdd00000.crgudc2
+
+#support seamless for QMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.platform.support.seamless=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.disable_rescue=true
+
+
+
+PRODUCT_PROPERTY_OVERRIDES += ro.llk.enable=false
+
+#tv path use video_tunnel
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.tv.fixed_tunnel=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.locale=en-US
+
+#Global Settings Key
+ifeq ($(ATV_LAUNCHER),amati)
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.global.settingskey=dashboard
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.global.settingskey=settings
+endif
