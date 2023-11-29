ed021e28bfc5a4e27d17a6d043f75b4ad4fada8c
Jian Wang <jian.wang@amlogic.com>
2023-11-29 14:07:02 +0800

    Media: CB1 s5 enable nr [1/1]
    PD#SWPL-147721
    
    Problem:
    S5 need enable nr default.
    
    Solution:
    Enable nr for s5
    
    Verify:
    s5
    
    Change-Id: I1e00e618282c91e72c9df4adb1eda1eda89f2938
    Signed-off-by: Jian Wang <jian.wang@amlogic.com>

--- products/mbox/s5/vendor_prop.mk
+++ products/mbox/s5/vendor_prop.mk
@@ -170,6 +170,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.c2.use_dmabufheaps=1 \
     debug.stagefright.c2inputsurface=-1 \
     debug.vendor.media.c2.vdec.support_10bit=false \
+    vendor.media.c2.disp.nr.enable=true \
+    vendor.media.c2.disp.di.localbuf_enable=true \
     vendor.media.c2.vdec.enable_h264_4k_mmu=true \
     ro.vendor.platform.support.8k=true
 endif
