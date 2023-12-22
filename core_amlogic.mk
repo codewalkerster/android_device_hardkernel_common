1ec498542ad707b22a777e2f9a613ee7fe247022
hong.wang <hong.wang@amlogic.com>
2023-12-22 17:34:34 +0800

    BT: add prop to fix rcu connect slowly [1/1]
    PD#SWPL-138831
    
    Problem:
    rcu reconnect slowly
    
    Solution:
    add bluetooth.core.le.connection_scan_interval_slow prop
    
    Verify:
    ohm
    
    Change-Id: I884f9194bf7113c5a291c37a727bcab570328029
    Signed-off-by: hong.wang <hong.wang@amlogic.com>

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -969,6 +969,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.profile.pan.nap.enabled=true \
     bluetooth.profile.pan.panu.enabled=true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.core.le.connection_scan_interval_slow=512
 # Disable Prime Shader Cache in SurfaceFlinger to make it available faster
 PRODUCT_PROPERTY_OVERRIDES += \
     service.sf.prime_shader_cache=0
