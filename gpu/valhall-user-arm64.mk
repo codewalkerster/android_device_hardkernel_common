8554b1adbb199a68ab26b9d89b4c45703cbae94b
lijun.zou <lijun.zou@amlogic.com>
2023-12-26 19:15:27 +0800

    gpu: update deqp level [1/1]
    PD#SWPL-150470
    
    Problem:
    VtsGpuTests fail
    
    Solution:
    update deqp level to the lastest
    
    Verify:
    t5m
    
    Change-Id: Ia9dd983ab346db7ced58d69ec45e57b638c33ab9
    Signed-off-by: lijun.zou <lijun.zou@amlogic.com>

--- gpu/valhall-user-arm64.mk
+++ gpu/valhall-user-arm64.mk
@@ -43,15 +43,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
 		frameworks/native/data/etc/android.hardware.opengles.aep.xml:vendor/etc/permissions/android.hardware.opengles.aep.xml \
-		frameworks/native/data/etc/android.software.opengles.deqp.level-2022-03-01.xml:vendor/etc/permissions/android.software.opengles.deqp.level-2022-03-01.xml \
+		frameworks/native/data/etc/android.software.opengles.deqp.level-2023-03-01.xml:vendor/etc/permissions/android.software.opengles.deqp.level-2023-03-01.xml \
 		frameworks/native/data/etc/android.hardware.vulkan.version-1_3.xml:vendor/etc/permissions/android.hardware.vulkan.version.xml \
 		frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:vendor/etc/permissions/android.hardware.vulkan.compute.xml \
 		frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:vendor/etc/permissions/android.hardware.vulkan.level.xml \
-		frameworks/native/data/etc/android.software.vulkan.deqp.level-2022-03-01.xml:vendor/etc/permissions/android.software.vulkan.deqp.level-2022-03-01.xml
+		frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:vendor/etc/permissions/android.software.vulkan.deqp.level-2023-03-01.xml
 else
 PRODUCT_COPY_FILES += \
 		frameworks/native/data/etc/android.hardware.opengles.aep.xml:vendor/etc/permissions/android.hardware.opengles.aep.xml \
-		frameworks/native/data/etc/android.software.opengles.deqp.level-2022-03-01.xml:vendor/etc/permissions/android.software.opengles.deqp.level-2022-03-01.xml
+		frameworks/native/data/etc/android.software.opengles.deqp.level-2023-03-01.xml:vendor/etc/permissions/android.software.opengles.deqp.level-2023-03-01.xml
 endif
 ifneq ($(wildcard hardware/amlogic/gralloc/gpu.xml),)
 PRODUCT_COPY_FILES += \
