12e89fa02a90145d86a1afafc6893754e78a8c50
wenbiao zhang <wenbiao.zhang@amlogic.com>
2024-01-12 16:22:42 +0800

    cuva: enable cuve module [2/2]
    PD#SWPL-151367
    
    Problem:
    enable cuve module
    
    Solution:
    enable cuve module
    
    Verify:
    bluebell
    
    Change-Id: I01fc210aff3bf9a7e27246f9cc583f6fc3ec2dcc
    Signed-off-by: wenbiao zhang <wenbiao.zhang@amlogic.com>

--- build_kernel_modules.mk
+++ build_kernel_modules.mk
@@ -179,6 +179,7 @@ VENDOR_KERNEL_MODULES += \
 include device/amlogic/common/tcon/tcon_modules.mk
 include device/amlogic/common/ldim/ldim_modules.mk
 include device/amlogic/common/video_algorithm/dnlp/dnlp_modules.mk
+include device/amlogic/common/video_algorithm/cuva/cuva_modules.mk
 include device/amlogic/common/video_algorithm/hdr10_tmo/hdr10_tmo_modules.mk
 include device/amlogic/common/video_algorithm/frc/frc_modules.mk
 include device/amlogic/common/soft_afbc/soft_afbc_modules.mk
