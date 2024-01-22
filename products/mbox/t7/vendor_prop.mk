990239074e9cfe11531261bb41ac946bc99a9f10
shuanshuan.jiang <shuanshuan.jiang@amlogic.com>
2024-01-22 20:11:27 +0800

    camera: only support 4k in T7C/S5 project [2/2]
    PD#SWPL-155247
    
    Problem:
    avoiding issue of low frame frate,only support 4k
    in T7c/S5 project
    
    Solution:
    using property control size
    
    Verify:
    local
    
    Change-Id: Idd7b6c128f7976f1f4d07c97f35ab5a40d023a78
    Signed-off-by: shuanshuan.jiang <shuanshuan.jiang@amlogic.com>

--- products/mbox/t7/vendor_prop.mk
+++ products/mbox/t7/vendor_prop.mk
@@ -23,7 +23,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.platform.need.display.hdmicec=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.media.camera_preview.maxsize=1920x1080 \
     ro.media.camera_preview.limitedrate=1920x1080x30,1280x720x30,640x480x30,320x240x28 \
     ro.vendor.platform.usehwmjpeg=true \
     vendor.media.camera.dec.mediahalsdk=true
