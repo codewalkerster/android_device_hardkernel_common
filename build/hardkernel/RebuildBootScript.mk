080aa2c89286aa73b73ed6ab907f20da81906b86
xiane <sangch.go@gmail.com>
2024-04-01 12:25:18 +0900

    ODROID-COMMON: boot script: Removed target-dtb.
    - Now dtb file is loaded from u-boot process. So it is unnecessary.
    - INI file is also loaded from u-boot process.
    
    Signed-off-by: xiane <sangch.go@gmail.com>
    Change-Id: Ie3ac06f048fb56e3b9e07e65f55f2cf75bbb9e70

--- build/hardkernel/RebuildBootScript.mk
+++ build/hardkernel/RebuildBootScript.mk
@@ -6,7 +6,6 @@ boot_part := 6
 recovery_part := 7
 wifi_country := US
 mtd := "sfc_nor:0x20000@0xe0000(env),0x200000@0x100000(uboot),0x100000@0x300000(splash),0xc00000@0x400000(firmware)"
-target_dtb := $(PRODUCT_KERNEL_DTS)
 target_board := $(PRODUCT_MODEL)
 emmc_boot_device := $(PRODUCT_BOOT_DEVICE)
 sd_boot_device := $(PRODUCT_SDMMC_DEVICE)
@@ -26,7 +25,6 @@ $(rebuild_bootscript) : $(PRODUCT_BOOTSCRIPT_TEMPLATE) $(HARDKERNEL_BOOTSCRIP_TO
 	--recovery-part $(recovery_part) \
 	--wifi-country $(wifi_country) \
 	--mtd $(mtd) \
-	--target-dtb $(target_dtb) \
 	--target-board $(target_board) \
 	--output $(rebuild_bootscript) \
 	--emmc-boot-device $(emmc_boot_device) \
--- scripts/bootscript_tools/bootscript_generator.py
+++ scripts/bootscript_tools/bootscript_generator.py
@@ -4,7 +4,7 @@ import getopt
 import os
 from string import Template
 
-usage = 'Invalid arguments. Example:\nbootscript_generator --variant userdebug --boot-part 6 --recovery-part 7 --wifi-country US --mtd flash.0:flash.0:0x1000@0x800(uboot),0x800@0x1800(splash),0x6000@0x2000(firmware) --target-dtb rk3568-odroid-m1 --target-board odroidm1 --output boot.scr --emmc-boot-device fe2e0000.mmc --sd-boot-device fe2c0000.mmc'
+usage = 'Invalid arguments. Example:\nbootscript_generator --variant userdebug --boot-part 6 --recovery-part 7 --wifi-country US --mtd flash.0:flash.0:0x1000@0x800(uboot),0x800@0x1800(splash),0x6000@0x2000(firmware) --target-board odroidm1 --output boot.scr --emmc-boot-device fe2e0000.mmc --sd-boot-device fe2c0000.mmc'
 
 def main(argv):
     infile = 'bootscript.in'
@@ -13,7 +13,6 @@ def main(argv):
     wifi_country = 'US'
     outfile = 'boot.cmd'
     mtd = "flash.0:0x1000@0x800(uboot),0x800@0x1800(splash),0x6000@0x2000(firmware)"
-    target_dtb = 'rk3568-odroid-m1'
     target_board = 'odroidm1'
     emmc_boot_device = 'fe310000.sdhci'
     sd_boot_device = 'fe2b0000.dwmmc'
@@ -40,8 +39,6 @@ def main(argv):
             outfile = arg
         elif opt == "--mtd":
             mtd = arg
-        elif opt == "--target-dtb":
-            target_dtb = arg
         elif opt == "--target-board":
             target_board = arg
         elif opt == "--emmc-boot-device":
@@ -64,7 +61,7 @@ def main(argv):
     template_bootscript_in = file_bootscript_in.read()
     template_in_t = Template(template_bootscript_in)
 
-    line = template_in_t.substitute(_variant=variant,_boot_part=boot_part,_recovery_part=recovery_part,_wifi_country=wifi_country, _mtd=mtd, _target_dtb=target_dtb, _target_board=target_board, _emmc_boot_device=emmc_boot_device, _sd_boot_device=sd_boot_device)
+    line = template_in_t.substitute(_variant=variant,_boot_part=boot_part,_recovery_part=recovery_part,_wifi_country=wifi_country, _mtd=mtd, _target_board=target_board, _emmc_boot_device=emmc_boot_device, _sd_boot_device=sd_boot_device)
 
     if outfile != '':
         with open (outfile,"w") as f:
