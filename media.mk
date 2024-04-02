be08d518d93747ecd9a71efc42f88cc68f9f7128
Jian Wang <jian.wang@amlogic.com>
2024-04-02 10:56:42 +0800

    Media: CB1 Fix tyson_atv can not play video [1/3]
    PD#SWPL-162450
    
    Problem:
    S5 have diff media ko, need use s5 init.rc
    
    Solution:
    Add TARGET_WITH_FB_MEDIA_MODULES to use s5 init.rc
    
    Verify:
    s5
    
    Change-Id: Iaa4f9cb194b6503ffd8242c4ceb05954096e8ec7
    Signed-off-by: Jian Wang <jian.wang@amlogic.com>

--- media.mk
+++ media.mk
@@ -434,15 +434,7 @@ BOARD_AML_HARDWARE_PATH := hardware/amlogic/
 AMLOGIC_FRAMEWORKS_AV_CONFIG_MK := $(BOARD_AML_VENDOR_PATH)/frameworks/av/mediaextconfig/config.mk
 BOARD_AML_MEDIA_HAL_CONFIG := $(BOARD_AML_MEDIAHAL_PATH)/media_base_config.mk
 
-ifeq ($(TARGET_PRODUCT),tyson)
-# for media modules
-PRODUCT_COPY_FILES += \
-	device/amlogic/common/initscripts/init.amlogic.media.s5.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifeq ($(TARGET_PRODUCT),tyson_mxl258c)
-# for media modules
-PRODUCT_COPY_FILES += \
-	device/amlogic/common/initscripts/init.amlogic.media.s5.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifneq (,$(findstring anemone,$(TARGET_PRODUCT)))
+ifeq ($(TARGET_WITH_FB_MEDIA_MODULES),true)
 # for media modules
 PRODUCT_COPY_FILES += \
 	device/amlogic/common/initscripts/init.amlogic.media.s5.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
