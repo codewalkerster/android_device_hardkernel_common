5ad6c7e7662dbc9291ed156c40501cfb9ca4a4a2
kuan.hu <kuan.hu@amlogic.com>
2024-04-08 10:59:59 +0800

    decoder: CF1 s6 use decoder h266 kernel modules [1/1]
    PD#SWPL-154192
    
    Problem:
    s6 add decoder h266 kernel modules
    
    Solution:
    insmod decoder s6 kernel modules
    
    Verify:
    s6
    
    Change-Id: I38b4a040b9f8f499023ea3393eb777427e717fb9
    Signed-off-by: kuan.hu <kuan.hu@amlogic.com>

--- media.mk
+++ media.mk
@@ -451,6 +451,22 @@ ifeq ($(TARGET_WITH_FB_MEDIA_MODULES),true)
 # for media modules
 PRODUCT_COPY_FILES += \
 	device/amlogic/common/initscripts/init.amlogic.media.s5.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
+else ifeq ($(TARGET_PRODUCT),raman)
+# for media modules
+PRODUCT_COPY_FILES += \
+	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
+else ifeq ($(TARGET_PRODUCT),raman_atv)
+# for media modules
+PRODUCT_COPY_FILES += \
+	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
+else ifeq ($(TARGET_PRODUCT),raman_hybrid)
+# for media modules
+PRODUCT_COPY_FILES += \
+	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
+else ifeq ($(TARGET_PRODUCT),raman_multidisplay)
+# for media modules
+PRODUCT_COPY_FILES += \
+	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
 else
 # for media modules
 PRODUCT_COPY_FILES += \
