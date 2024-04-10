64d6f5c7ccc6792c908a2f81448afc8be82894d1
wei wang <wei.wang@amlogic.com>
2024-04-10 14:07:18 +0800

    property: remove unused properties [1/1]
    PD#SWPL-163899
    
    Problem:
    some properties were set failed because of selinux
    
    Solution:
    remove them
    
    Verify:
    Verify it on t7_an400
    
    Change-Id: Icd2816698b5d944b33278be3dc5923dbf6450d09
    Signed-off-by: wei wang <wei.wang@amlogic.com>
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- products/mbox/s5/vendor_prop.mk
+++ products/mbox/s5/vendor_prop.mk
@@ -85,11 +85,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.volume.filenames_mode=aes-256-cts
 
-# hwui
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=40.5f \
-    ro.hwui.layer_cache_size=33.75f
-
 # default enable sdr to hdr
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sdr2hdr.enable=true
--- products/tv/t5w/vendor_prop.mk
+++ products/tv/t5w/vendor_prop.mk
@@ -38,10 +38,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     #ro.media.camera_preview.limitedrate=1280x720x30,640x480x30,320x240x28
 
 #camera max to 1080p
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.media.camera_preview.maxsize=1920x1080 \
-    ro.media.camera_preview.limitedrate=1920x1080x30,1280x720x30,640x480x30,320x240x28 \
-    ro.media.camera_preview.usemjpeg=1
+#PRODUCT_PRODUCT_PROPERTIES += \
+#    ro.media.camera_preview.maxsize=1920x1080 \
+#    ro.media.camera_preview.limitedrate=1920x1080x30,1280x720x30,640x480x30,320x240x28 \
+#    ro.media.camera_preview.usemjpeg=1
 
 
 
