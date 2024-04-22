6bcb04c0a5fa4045bd230d194029edbd259f5dbb
Chris <codewalker@hardkernel.com>
2024-04-22 12:43:34 +0900

    ODROID: Rollback b/t firmware and firmware_class.path.
    Change-Id: If59ebaee15bbe1080c5881bc6da9e2fc5e6e5436

--- modules/bluetooth.mk
+++ modules/bluetooth.mk
@@ -76,8 +76,8 @@ endif
 ifeq ($(strip $(BOARD_HAVE_BLUETOOTH_RTK)), true)
 include hardware/realtek/rtkbt/rtkbt.mk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/../bluetooth/rtl8821c_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl_bt/rtl8821c_fw.bin \
-    $(LOCAL_PATH)/../bluetooth/rtl8821c_config.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl_bt/rtl8821c_config.bin
+    $(LOCAL_PATH)/../bluetooth/rtl8821c_fw.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl_bt/rtl8821c_fw.bin \
+    $(LOCAL_PATH)/../bluetooth/rtl8821c_config.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl_bt/rtl8821c_config.bin
 endif
 
 ifeq ($(strip $(BOARD_HAVE_BLUETOOTH_AIC)), true)
