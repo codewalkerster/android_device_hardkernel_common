e6e5d64199e2f25e4c92e3d016cfb0e879e30230
gongwei.chen <gongwei.chen@amlogic.com>
2024-06-05 17:28:39 +0800

    s6: add s6 support [2/4]
    PD#SWPL-172133
    
    Problem:
    need add s6 support
    
    Solution:
    add s6 support
    
    Verify:
    verify by raman
    
    Change-Id: Ib7b21c012bb3d6860cb589419d7191ae5098595e
    Signed-off-by: gongwei.chen <gongwei.chen@amlogic.com>

---
+++ products/mbox/s6/device.mk
@@ -0,0 +1,97 @@
+#
+# Copyright (C) 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+CHIP_DIR := device/amlogic/common/products/mbox/s6
+
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/products/mbox/Vendor_1915_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1915_Product_0001.kl
+
+#use tv remote layout for mbox if livetv is built
+ifeq ($(TARGET_BUILD_LIVETV), true)
+    PRODUCT_COPY_FILES += \
+       device/amlogic/common/products/mbox/Vendor_0001_Product_0002.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
+else ifeq ($(SUPPORT_CBS), true)
+    PRODUCT_COPY_FILES += \
+       device/amlogic/common/products/mbox/Vendor_0001_Product_0002.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
+else
+    PRODUCT_COPY_FILES += \
+       device/amlogic/common/products/mbox/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
+endif
+
+
+#########################################################################
+#
+# Init config
+#
+#########################################################################
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/products/mbox/init.amlogic.system.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.rc
+
+
+ifneq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/recovery/init.recovery.amlogic.rc:recovery/root/init.recovery.amlogic.rc
+else
+PRODUCT_COPY_FILES += \
+    device/amlogic/common/recovery/init.recovery.amlogic_ab.rc:recovery/root/init.recovery.amlogic.rc
+endif
+
+#########################################################################
+#
+# Media codec
+#
+#########################################################################
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    $(CHIP_DIR)/files/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
+
+#########################################################################
+#
+# PQ
+#
+#########################################################################
+ifeq ($(TARGET_PRODUCT),oppen_mxl258c)
+TVCONFIG_FILES := \
+    $(CHIP_DIR)/files/tv/tvconfig_fccpip/*
+else
+TVCONFIG_FILES := \
+    $(CHIP_DIR)/files/tv/tvconfig/*
+endif
+
+PQ_FILES := \
+    $(CHIP_DIR)/files/PQ/pq.db \
+    $(CHIP_DIR)/files/PQ/pq_default.ini \
+    $(CHIP_DIR)/files/PQ/UI_PQSetting.bin
+
+
+#thermal 2.0 config file
+ifeq ($(TARGET_BUILD_KERNEL_VERSION),5.15)
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/thermal_info_config_5_15.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+else
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+endif
+#########################################################################
+#
+# tunerhal
+#
+#########################################################################
+ifeq ($(SUPPORT_TUNERHAL), true)
+PRODUCT_COPY_FILES += \
+    $(CHIP_DIR)/files/tunerhal/frontendinfos.json:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_hal/frontendinfos.json
+endif
