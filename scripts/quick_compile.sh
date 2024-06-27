80d4815dd7057314048e5421c9527fb87b6d6a72
gongwei.chen <gongwei.chen@amlogic.com>
2024-06-27 11:31:14 +0800

    add raman multidisplay for S905D5 [2/2]
    PD#SWPL-175045
    
    Problem:
    need add raman multidisplay for S905D5
    
    Solution:
    add raman multidisplay for S905D5
    
    Verify:
    verify by raman
    
    Change-Id: Ia5d4eef03d2bb4eb0b7902cea48a64874849bebb
    Signed-off-by: gongwei.chen <gongwei.chen@amlogic.com>

--- scripts/quick_compile.sh
+++ scripts/quick_compile.sh
@@ -728,11 +728,11 @@ kernel_exec[58]="./mk raman -v "
 ###########################################################################################
 
 ###########################################################################################
-# RAMAN S905D5 GTV
+# RAMAN MULTI DISPLAY GTV
 project[59]="RAMAN_S905D5_GTV"
 soc[59]="S905D5"
 hardware[59]="BQ201"
-module[59]="raman"
+module[59]="raman_multidisplay"
 uboot_path[59]="device/amlogic/raman"
 uboot_exec_aosp[59]="./mk s6_bq201 --vab --avb2 --fastboot-write"
 uboot_exec_drm[59]="./mk s6_bq201 --vab --avb2 --fastboot-write"
@@ -741,11 +741,11 @@ kernel_exec[59]="./mk raman -v "
 ###########################################################################################
 
 ###########################################################################################
-# RAMAN S905D5 SKT
+# RAMAN MULTI DISPLAY SKT
 project[60]="RAMAN_S905D5_SKT"
 soc[60]="S905D5"
 hardware[60]="BQ208"
-module[60]="raman"
+module[60]="raman_multidisplay"
 uboot_path[60]="device/amlogic/raman"
 uboot_exec_aosp[60]="./mk s6_bq208 --vab --avb2 --fastboot-write"
 uboot_exec_drm[60]="./mk s6_bq208 --vab --avb2 --fastboot-write"
