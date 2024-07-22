318d973037af8afcec892e36401c9fb325878637
qiuye.gan <qiuye.gan@amlogic.com>
2024-07-22 16:26:15 +0800

    media.mk: CF2 add property to support vcc for tsplayer. [2/2]
    PD#SWPL-178350
    
    Problem:
    add property to support vcc for tsplayer.
    
    Solution:
    add property to support vcc for tsplayer.
    
    Verify:
    raman
    
    Change-Id: Id54f66e2cd3773e340e2cb0a66df4d1a07fed787
    Signed-off-by: qiuye.gan <qiuye.gan@amlogic.com>

--- media.mk
+++ media.mk
@@ -291,6 +291,18 @@ PRODUCT_PACKAGES += \
 
 endif
 
+#support vcc
+ifeq ($(TARGET_VENDOR_MEDIA_VVC_SUPPORT),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.mediahal.tsplayer.support_vcc=1
+else ifeq ($(TARGET_VENDOR_MEDIA_VVC_SUPPORT), false)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.mediahal.tsplayer.support_vcc=0
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.media.mediahal.tsplayer.support_vcc=1
+endif
+
 #########################################################################
 #
 #                                    Resman json
