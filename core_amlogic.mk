d036baa7affb505f59c32ca5b7c15a2827c93bed
hong.wang <hong.wang@amlogic.com>
2024-07-30 18:57:03 +0800

    BT: aosp bt-stack no sync google btif_av_src_sink_coexist_enabled [1/1]
    PD#SWPL-178773
    
    Problem:
    aosp bt can not connect speaker
    
    Solution:
    add a2dp config
    
    Verify:
    ohm
    
    Change-Id: I2bc506445b9d1e7b9a4ad290974fbaa56a51bad4
    Signed-off-by: hong.wang <hong.wang@amlogic.com>

--- core_amlogic.mk
+++ core_amlogic.mk
@@ -1055,6 +1055,7 @@ PRODUCT_PACKAGES += \
     MtpService
 
 # Set supported Bluetooth profiles to enabled
+ifeq ($(BOARD_COMPILE_ATV),true)
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.profile.a2dp.sink.enabled=true \
     bluetooth.profile.avrcp.controller.enabled=true \
@@ -1062,6 +1063,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.profile.avrcp.target.enabled=true \
     bluetooth.profile.hfp.hf.enabled=true \
     bluetooth.profile.hfp.ag.enabled=true
+else
+ifeq ($(BOARD_ENABLE_A2DP_SINK),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.profile.a2dp.sink.enabled=true \
+    bluetooth.profile.avrcp.controller.enabled=true \
+    bluetooth.profile.hfp.hf.enabled=true
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true
+endif
+endif
 
 ifeq ($(BOARD_ENABLE_LE_AUDIO),true)
 PRODUCT_PROPERTY_OVERRIDES += \
