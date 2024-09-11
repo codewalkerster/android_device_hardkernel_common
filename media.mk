985b2fb2b70eec33cfda3a59fecb5e4dd5590573
kuan.hu <kuan.hu@amlogic.com>
2024-09-11 17:01:23 +0800

    Media: CB2 Adjust S6 media ko loading script [2/2]
    PD#SWPL-185338
    
    Problem:
    S6 have diff media ko, need use s6 init.rc
    
    Solution:
    Add TARGET_WITH_S6_MEDIA_MODULES to use s6 init.rc
    
    Verify:
    s6
    
    Change-Id: I90841c035831151587a42ef3a2a398504faabd08
    Signed-off-by: kuan.hu <kuan.hu@amlogic.com>

--- media.mk
+++ media.mk
@@ -473,30 +473,10 @@ ifeq ($(TARGET_WITH_FB_MEDIA_MODULES),true)
 # for media modules
 PRODUCT_COPY_FILES += \
 	device/amlogic/common/initscripts/init.amlogic.media.s5.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifeq ($(TARGET_PRODUCT),raman)
+else ifeq ($(TARGET_WITH_S6_MEDIA_MODULES),true)
 # for media modules
 PRODUCT_COPY_FILES += \
 	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifeq ($(TARGET_PRODUCT),raman_atv)
-# for media modules
-PRODUCT_COPY_FILES += \
-	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifeq ($(TARGET_PRODUCT),raman_hybrid)
-# for media modules
-PRODUCT_COPY_FILES += \
-	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifeq ($(TARGET_PRODUCT),raman_arm64)
-# for media modules
-PRODUCT_COPY_FILES += \
-	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifeq ($(TARGET_PRODUCT),raman_multidisplay)
-# for media modules
-PRODUCT_COPY_FILES += \
-	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
-else ifeq ($(TARGET_PRODUCT),raman_multidisplay_arm64)
-# for media modules
-PRODUCT_COPY_FILES += \
-	device/amlogic/common/initscripts/init.amlogic.media.s6.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc	
 else
 # for media modules
 PRODUCT_COPY_FILES += \
