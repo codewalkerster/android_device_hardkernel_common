f2390950bbf419c7e23ac9eafa5954ec2cc45a34
codewalkerster <codewalker@hardkernel.com>
2025-02-11 16:41:46 +0900

    ODROID-C5: Make Tablet device in Google Play Store.
    Change-Id: I5532919b5fa9dc64b606caf9341fa66d6a0355a7

--- device.mk
+++ device.mk
@@ -1,2 +1,6 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/hardkernel-720.bmp.gz:$(TARGET_OUT)/boot-logo.bmp.gz
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml
