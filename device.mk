3b9dee70ec7e78d0aba938c4a2283e5dd93f0623
xiane <sangch.go@gmail.com>
2025-03-24 09:17:03 +0900

    ODROID: things: Add wiringPi packages.
    Signed-off-by: xiane <sangch.go@gmail.com>
    Change-Id: If9eabf9d1c45f56ac59cd96754413747f2f8939e

--- device.mk
+++ device.mk
@@ -4,3 +4,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml
+
+#wiringPi
+PRODUCT_PACKAGES += \
+    libwiringPi \
+    libwiringPiDev \
+    gpio
