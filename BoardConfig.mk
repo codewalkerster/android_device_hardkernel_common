db934850c946ce81f037d0afffb45d86c9faaa2e
xiane <sangch.go@gmail.com>
2025-04-08 14:15:32 +0900

    ODROID-C5: common: Add can utils.
    Signed-off-by: xiane <sangch.go@gmail.com>
    Change-Id: Iba9b761d8c36326f72119f4344b425eb6199be24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,6 @@
 PRODUCT_BOOTSCRIPT_TEMPLATE ?= device/hardkernel/common/scripts/bootscript_tools/bootscript.in
 PRODUCT_BOOTSCRIPT_DTB_TEMPLATE ?= device/hardkernel/common/scripts/bootscript_tools/bootscript_dtb.in
 PRODUCT_BOOTSCRIPT_INI_TEMPLATE ?= device/hardkernel/common/scripts/bootscript_tools/bootscript_ini.in
+
+# Can utils
+include device/hardkernel/common/can_utils.mk
